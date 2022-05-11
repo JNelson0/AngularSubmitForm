1dc24a540730d6c8061c7a5d5970b36fe2eb13de
Jared Nelson <43326406+JNelson0@users.noreply.github.com>
2022-05-10 22:02:14 -0700

    Setup

---
+++ server/prisma/migrations/20220507074103_user_migrate/migration.sql
@@ -0,0 +1,20 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('USER', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "subscription" TEXT NOT NULL,
+    "passwordSalt" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_passwordSalt_key" ON "User"("passwordSalt");
