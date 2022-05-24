10d8673d7b2618531258a85e8361e8c5cf1208aa
Jared Nelson <jaredlnelson1@gmail.com>
2022-05-23 18:21:51 -0700

    Add one test case and change ui

--- client/src/app/user-register.service.spec.ts
+++ client/src/app/user-register.service.spec.ts
@@ -1,4 +1,4 @@
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClientModule, HttpErrorResponse } from "@angular/common/http";
 import { TestBed } from "@angular/core/testing";
 
 import { UserRegisterService } from "./user-register.service";
@@ -17,6 +17,13 @@ describe("UserRegisterService", () => {
         expect(service).toBeTruthy();
     });
 
+    it("handle error", () => {
+        service
+            .handleError(new HttpErrorResponse({ error: "bar", status: 403 }))
+            .subscribe((value) => {
+                expect(typeof value).toBe("error");
+            });
+    });
     // it("getUsers should return object", (done: DoneFn) => {
     //     service.getUsers().subscribe((value) => {
     //         expect(typeof value).toBe("object");
